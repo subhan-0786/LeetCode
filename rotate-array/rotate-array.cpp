42b7ce89ac9d01fbcd0cf857f2a6f5e712689421
Subhan Amjad <124813748+subhan-0786@users.noreply.github.com>
2024-08-23 03:06:20 +0500

    Update rotate-array.cpp

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -7,3 +7,24 @@ public:
         reverse(nums.begin()+k, nums.end());
     }
 };
+
+// This code also works for most pf the cases but not all
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         k = k % n; // if k > size
+
+//         if (k == 0) return; 
+        
+//         int i, j;
+
+//         for (i = 0; i <= k; i++) {
+//             int temp = nums[i];
+//             for (j = 0; j < n-1; j++)
+//                 nums[j] = nums[j+1];
+//             nums[j] = temp;
+//         }
+//     }
+// };
